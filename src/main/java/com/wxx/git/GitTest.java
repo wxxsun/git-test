743369d9f5acdc5d6d972a2cd96301fb0e80d3ed
wufang <1638069860@qq.com>
2022-03-09 21:40:03 +0800

    first commit

---
+++ src/main/java/com/wxx/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wxx.git;
+
+/**
+ * @author wufang
+ * @date 2022/3/9 -21:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
