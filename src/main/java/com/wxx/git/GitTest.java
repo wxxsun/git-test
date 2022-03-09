2358873724a946fc2feb06489a7459ec6c7b6645
wufang <1638069860@qq.com>
2022-03-09 22:16:41 +0800

    333 commit

--- src/main/java/com/wxx/git/GitTest.java
+++ src/main/java/com/wxx/git/GitTest.java
@@ -7,5 +7,8 @@ package com.wxx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
